bc819b4d3e5452f158b16b9ce81bde02e3dcdcf4
Jeff Mesnil <jmesnil@gmail.com>
2008-08-01 18:19:09 +0200

    added an example about using a TabularData attribute

---
+++ examples/runtime_sysprops.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env jruby
+require 'rubygems'
+require 'jmx4r'
+
+# This example shows how to iterate on TabularData
+
+runtime = JMX::MBean.find_by_name "java.lang:type=Runtime"
+
+# The system_properties attribute of the Runtime MBean is an instance of
+#   TabularDataSupport
+sysprops = runtime.system_properties
+sysprops.values.each do | sysprop|
+  puts "#{sysprop["key"]} = #{sysprop["value"]}"
+end
