57e46d10dbba010fd7a9c672cbe313f5c617a015
Jeff Mesnil <jmesnil@gmail.com>
2009-10-20 09:45:06 +0200

    added tests for operation invocations

---
+++ test/tc_methods.rb
@@ -0,0 +1,24 @@
+# Copyright 2007 Jeff Mesnil (http://jmesnil.net)
+
+require "test/unit"
+
+require "jmx4r"
+require "jconsole"
+
+class TestMethods < Test::Unit::TestCase
+  java_import java.lang.management.ManagementFactory
+
+  def setup
+    @logging = JMX::MBean.find_by_name "java.util.logging:type=Logging", :connection => ManagementFactory.platform_mbean_server
+  end
+
+  def teardown
+    JMX::MBean.remove_connection
+  end
+
+  def test_invoke_operation
+    @logging.set_logger_level "global", "FINEST"
+    assert_equal "FINEST", @logging.get_logger_level("global")
+  end
+
+end
--- test/ts_all.rb
+++ test/ts_all.rb
@@ -6,6 +6,7 @@ require "tc_connection"
 require "tc_auth"
 require "tc_multiple_connections"
 require "tc_attributes"
+require "tc_methods"
 require "tc_composite_data"
 require "tc_dynamic_mbean"
 
